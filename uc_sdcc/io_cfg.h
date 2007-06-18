4775fd881b9e2de668ae8cf180e893c83222a70b
fransschreuder <fransschreuder@29735491-9d32-0410-b17f-ed4c7e910dea>
2007-06-18 12:21:11 +0000

    git-svn-id: https://usbpicprog.svn.sourceforge.net/svnroot/usbpicprog@28 29735491-9d32-0410-b17f-ed4c7e910dea

---
+++ uc_sdcc/io_cfg.h
@@ -0,0 +1,111 @@
+/*********************************************************************
+ *
+ *                Microchip USB C18 Firmware Version 1.0
+ *
+ *********************************************************************
+ * FileName:        io_cfg.h
+ * Dependencies:    See INCLUDES section below
+ * Processor:       PIC18
+ * Compiler:        C18 2.30.01+
+ * Company:         Microchip Technology, Inc.
+ *
+ * Software License Agreement
+ *
+ * The software supplied herewith by Microchip Technology Incorporated
+ * (the “Company”) for its PICmicro® Microcontroller is intended and
+ * supplied to you, the Company’s customer, for use solely and
+ * exclusively on Microchip PICmicro Microcontroller products. The
+ * software is owned by the Company and/or its supplier, and is
+ * protected under applicable copyright laws. All rights are reserved.
+ * Any use in violation of the foregoing restrictions may subject the
+ * user to criminal sanctions under applicable laws, as well as to
+ * civil liability for the breach of the terms and conditions of this
+ * license.
+ *
+ * THIS SOFTWARE IS PROVIDED IN AN “AS IS” CONDITION. NO WARRANTIES,
+ * WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
+ * TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
+ * IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
+ * CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ *
+ * Author               Date        Comment
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Rawin Rojvanit       7/21/04     Original.
+ ********************************************************************/
+
+#ifndef IO_CFG_H
+#define IO_CFG_H
+
+/** I N C L U D E S *************************************************/
+#include "autofiles\usbcfg.h"
+
+/** T R I S *********************************************************/
+#define INPUT_PIN           1
+#define OUTPUT_PIN          0
+
+/** U S B ***********************************************************/
+#define tris_usb_bus_sense  TRISAbits.TRISA1    // Input
+
+#if defined(USE_USB_BUS_SENSE_IO)
+#define usb_bus_sense       PORTAbits.RA1
+#else
+#define usb_bus_sense       1
+#endif
+
+#define tris_self_power     TRISAbits.TRISA2    // Input
+
+#if defined(USE_SELF_POWER_SENSE_IO)
+#define self_power          PORTAbits.RA2
+#else
+#define self_power          1
+#endif
+
+/** L E D ***********************************************************/
+#define mInitAllLEDs()      LATD &= 0xF0; TRISD &= 0xF0;
+
+#define mLED_1              LATDbits.LATD0
+#define mLED_2              LATDbits.LATD1
+#define mLED_3              LATDbits.LATD2
+#define mLED_4              LATDbits.LATD3
+
+#define mLED_1_On()         mLED_1 = 1;
+#define mLED_2_On()         mLED_2 = 1;
+#define mLED_3_On()         mLED_3 = 1;
+#define mLED_4_On()         mLED_4 = 1;
+
+#define mLED_1_Off()        mLED_1 = 0;
+#define mLED_2_Off()        mLED_2 = 0;
+#define mLED_3_Off()        mLED_3 = 0;
+#define mLED_4_Off()        mLED_4 = 0;
+
+#define mLED_1_Toggle()     mLED_1 = !mLED_1;
+#define mLED_2_Toggle()     mLED_2 = !mLED_2;
+#define mLED_3_Toggle()     mLED_3 = !mLED_3;
+#define mLED_4_Toggle()     mLED_4 = !mLED_4;
+
+/** S W I T C H *****************************************************/
+#define mInitAllSwitches()  TRISBbits.TRISB4=1;TRISBbits.TRISB5=1;
+#define mInitSwitch2()      TRISBbits.TRISB4=1;
+#define mInitSwitch3()      TRISBbits.TRISB5=1;
+#define sw2                 PORTBbits.RB4
+#define sw3                 PORTBbits.RB5
+
+/** P O T ***********************************************************/
+#define mInitPOT()          TRISAbits.TRISA0=1;ADCON0=0x01;ADCON2=0x3C;
+
+/** S P I : Chip Select Lines ***************************************/
+#define tris_cs_temp_sensor TRISBbits.TRISB2    // Output
+#define cs_temp_sensor      LATBbits.LATB2
+
+#define tris_cs_sdmmc       TRISBbits.TRISB3    // Output
+#define cs_sdmmc            LATBbits.LATB3
+
+/** S D M M C *******************************************************/
+#define TRIS_CARD_DETECT    TRISBbits.TRISB4    // Input
+#define CARD_DETECT         PORTBbits.RB4
+
+#define TRIS_WRITE_DETECT   TRISAbits.TRISA4    // Input
+#define WRITE_DETECT        PORTAbits.RA4
+
+#endif //IO_CFG_H
---
+++ uc_sdcc/user/user.c
@@ -0,0 +1,255 @@
+/*********************************************************************
+ *
+ *                Microchip USB C18 Firmware Version 1.0
+ *
+ *********************************************************************
+ * FileName:        user.c
+ * Dependencies:    See INCLUDES section below
+ * Processor:       PIC18
+ * Compiler:        C18 2.30.01+
+ * Company:         Microchip Technology, Inc.
+ *
+ * Software License Agreement
+ *
+ * The software supplied herewith by Microchip Technology Incorporated
+ * (the “Company”) for its PICmicro® Microcontroller is intended and
+ * supplied to you, the Company’s customer, for use solely and
+ * exclusively on Microchip PICmicro Microcontroller products. The
+ * software is owned by the Company and/or its supplier, and is
+ * protected under applicable copyright laws. All rights are reserved.
+ * Any use in violation of the foregoing restrictions may subject the
+ * user to criminal sanctions under applicable laws, as well as to
+ * civil liability for the breach of the terms and conditions of this
+ * license.
+ *
+ * THIS SOFTWARE IS PROVIDED IN AN “AS IS” CONDITION. NO WARRANTIES,
+ * WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
+ * TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
+ * IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
+ * CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ *
+ * Author               Date        Comment
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Rawin Rojvanit       11/19/04    Original.
+ ********************************************************************/
+
+/******************************************************************************
+ * CDC RS-232 Emulation Tutorial Instructions:
+ ******************************************************************************
+ * Refer to Application Note AN956 for explanation of the CDC class.
+ * 
+ * First take a look at Exercise_Example() and study how functions are called.
+ * 
+ * There are five exercises, each one has a solution in the CDC\user\solutions.
+ * Scroll down and look for Exercise_01,_02,_03,_04, and _05.
+ * Instructions on what to do is inside each function.
+ * 
+ *****************************************************************************/
+
+/** I N C L U D E S **********************************************************/
+#include <p18cxxx.h>
+#include <usart.h>
+#include "system\typedefs.h"
+
+#include "system\usb\usb.h"
+
+#include "io_cfg.h"             // I/O pin mapping
+#include "user\user.h"
+
+
+/** V A R I A B L E S ********************************************************/
+#pragma udata
+byte old_sw2,old_sw3;
+
+char input_buffer[64];
+char output_buffer[64];
+
+rom char welcome[]={"UsbPicProg - Open Source USB PIC programmer\r\n\r\n"};
+rom char ansi_clrscr[]={"\x1b[2J"};         // ANSI Clear Screen Command
+
+/** P R I V A T E  P R O T O T Y P E S ***************************************/
+void BlinkUSBStatus(void);
+BOOL Switch2IsPressed(void);
+BOOL Switch3IsPressed(void);
+
+
+/** D E C L A R A T I O N S **************************************************/
+#pragma code
+void UserInit(void)
+{
+    mInitAllLEDs();
+    mInitAllSwitches();
+    old_sw2 = sw2;
+    old_sw3 = sw3;
+    
+
+    
+
+
+}//end UserInit
+
+
+/******************************************************************************
+ * Function:        void ProcessIO(void)
+ *
+ * PreCondition:    None
+ *
+ * Input:           None
+ *
+ * Output:          None
+ *
+ * Side Effects:    None
+ *
+ * Overview:        This function is a place holder for other user routines.
+ *                  It is a mixture of both USB and non-USB tasks.
+ *
+ * Note:            None
+ *****************************************************************************/
+void ProcessIO(void)
+{   
+    BlinkUSBStatus();
+    // User Application USB tasks
+    if((usb_device_state < CONFIGURED_STATE)||(UCONbits.SUSPND==1)) return;
+
+    Exercise_Example();
+
+
+}//end ProcessIO
+
+void Exercise_Example(void)
+{
+    static byte start_up_state = 0;
+    
+    if(start_up_state == 0)
+    {
+        if(Switch2IsPressed())
+            start_up_state++;
+    }
+    else if(start_up_state == 1)
+    {
+        if(mUSBUSARTIsTxTrfReady())
+        {
+            putrsUSBUSART(ansi_clrscr);
+            start_up_state++;
+        }
+    }
+    else if(start_up_state == 2)
+    {
+        if(mUSBUSARTIsTxTrfReady())
+        {
+            putrsUSBUSART("\rMicrochip Technology Inc., 2004\r\n");
+            start_up_state++;
+        }
+    }
+    else if(start_up_state == 3)
+    {
+        if(mUSBUSARTIsTxTrfReady())
+        {
+            putrsUSBUSART(welcome);
+            start_up_state++;
+        }
+    }
+    
+}//end Exercise_Example
+
+
+/******************************************************************************
+ * Function:        void BlinkUSBStatus(void)
+ *
+ * PreCondition:    None
+ *
+ * Input:           None
+ *
+ * Output:          None
+ *
+ * Side Effects:    None
+ *
+ * Overview:        BlinkUSBStatus turns on and off LEDs corresponding to
+ *                  the USB device state.
+ *
+ * Note:            mLED macros can be found in io_cfg.h
+ *                  usb_device_state is declared in usbmmap.c and is modified
+ *                  in usbdrv.c, usbctrltrf.c, and usb9.c
+ *****************************************************************************/
+void BlinkUSBStatus(void)
+{
+    static word led_count=0;
+    
+    if(led_count == 0)led_count = 10000U;
+    led_count--;
+
+    #define mLED_Both_Off()         {mLED_1_Off();mLED_2_Off();}
+    #define mLED_Both_On()          {mLED_1_On();mLED_2_On();}
+    #define mLED_Only_1_On()        {mLED_1_On();mLED_2_Off();}
+    #define mLED_Only_2_On()        {mLED_1_Off();mLED_2_On();}
+
+    if(UCONbits.SUSPND == 1)
+    {
+        if(led_count==0)
+        {
+            mLED_1_Toggle();
+            mLED_2 = mLED_1;        // Both blink at the same time
+        }//end if
+    }
+    else
+    {
+        if(usb_device_state == DETACHED_STATE)
+        {
+            mLED_Both_Off();
+        }
+        else if(usb_device_state == ATTACHED_STATE)
+        {
+            mLED_Both_On();
+        }
+        else if(usb_device_state == POWERED_STATE)
+        {
+            mLED_Only_1_On();
+        }
+        else if(usb_device_state == DEFAULT_STATE)
+        {
+            mLED_Only_2_On();
+        }
+        else if(usb_device_state == ADDRESS_STATE)
+        {
+            if(led_count == 0)
+            {
+                mLED_1_Toggle();
+                mLED_2_Off();
+            }//end if
+        }
+        else if(usb_device_state == CONFIGURED_STATE)
+        {
+            if(led_count==0)
+            {
+                mLED_1_Toggle();
+                mLED_2 = !mLED_1;       // Alternate blink                
+            }//end if
+        }//end if(...)
+    }//end if(UCONbits.SUSPND...)
+
+}//end BlinkUSBStatus
+
+BOOL Switch2IsPressed(void)
+{
+    if(sw2 != old_sw2)
+    {
+        old_sw2 = sw2;                  // Save new value
+        if(sw2 == 0)                    // If pressed
+            return TRUE;                // Was pressed
+    }//end if
+    return FALSE;                       // Was not pressed
+}//end Switch2IsPressed
+
+BOOL Switch3IsPressed(void)
+{
+    if(sw3 != old_sw3)
+    {
+        old_sw3 = sw3;                  // Save new value
+        if(sw3 == 0)                    // If pressed
+            return TRUE;                // Was pressed
+    }//end if
+    return FALSE;                       // Was not pressed
+}//end Switch3IsPressed
+
+/** EOF user.c ***************************************************************/
