48797b5b5b10278b3f3d0819cac28431f695950c
fransschreuder <fransschreuder@29735491-9d32-0410-b17f-ed4c7e910dea>
2008-10-30 18:38:59 +0000

    added script to install all packages in order to be able to build usbpicprog for ubuntu / debian
    git-svn-id: https://usbpicprog.svn.sourceforge.net/svnroot/usbpicprog@379 29735491-9d32-0410-b17f-ed4c7e910dea

---
+++ trunk/upp_wx/get_build_packages_ubuntu.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install build-essential wx2.8-headers libusb-dev libwxbase2.8-dev libwxbase2.8-0 libwxgtk2.8-0 libwxgtk2.8-dev md5deep autoconf automake autotools-dev 
+
+
