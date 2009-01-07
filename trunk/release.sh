c96d4e59b10f353f27aacb6e0594dd996495162f
fransschreuder <fransschreuder@29735491-9d32-0410-b17f-ed4c7e910dea>
2009-01-07 08:02:10 +0000

    automatic version / date string in release script
    git-svn-id: https://usbpicprog.svn.sourceforge.net/svnroot/usbpicprog@508 29735491-9d32-0410-b17f-ed4c7e910dea

--- trunk/release.sh
+++ trunk/release.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+
+if [ $# -ne 0 ]
+then
+  RELEASE=$1
+else
+  RELEASE=$(svnversion -n)-$(date +%Y%m%d)
+fi
+echo Building release: $RELEASE
+
 if [ -d release ]
 then
 	echo Directory release exists, removing ...
@@ -10,22 +20,22 @@ mkdir release/usbpicprog
 mkdir release/firmware
 
 echo exporting svn tree...
-svn export uc_code release/firmware/firmware-$1
+svn export uc_code release/firmware/firmware-$RELEASE
 svn export boot release/firmware/boot1.0
-svn export upp_wx release/usbpicprog/usbpicprog-$1
+svn export upp_wx release/usbpicprog/usbpicprog-$RELEASE
 
 echo copy hexfiles...
-cp release/firmware/firmware-$1/uc_code.hex release/firmware/firmware-$1.hex
+cp release/firmware/firmware-$RELEASE/uc_code.hex release/firmware/firmware-$RELEASE.hex
 cp release/firmware/boot1.0/Boot.hex release/firmware/boot1.0.hex
 
 echo making firmware tar.gz archive...
 cd release/firmware
-tar -zcvhf firmware-$1.tar.gz *
-mv firmware-$1.tar.gz ..
+tar -zcvhf firmware-$RELEASE.tar.gz *
+mv firmware-$RELEASE.tar.gz ..
 cd ..
 
 echo creating autogen files
-cd usbpicprog/usbpicprog-$1
+cd usbpicprog/usbpicprog-$RELEASE
 ./autogen.sh
 cp po/*.mo ..
 make distclean
@@ -35,13 +45,13 @@ rm -rf m4
 rm -rf autom4te.cache
 cd ..
 echo creating source archive...
-tar -zcvhf usbpicprog-$1.tar.gz usbpicprog-$1
-mv usbpicprog-$1.tar.gz ..
+tar -zcvhf usbpicprog-$RELEASE.tar.gz usbpicprog-$RELEASE
+mv usbpicprog-$RELEASE.tar.gz ..
 
-cd usbpicprog-$1
+cd usbpicprog-$RELEASE
 echo creating deb package...
 ./mkdeb.sh
-mv usbpicprog0.2.0.deb ../../usbpicprog-$1.deb
+mv usbpicprog0.2.0.deb ../../usbpicprog-$RELEASE.deb
 cd ../..
 rm -rf firmware
 rm -rf usbpicprog
