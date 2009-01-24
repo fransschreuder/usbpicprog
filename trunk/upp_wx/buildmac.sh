38aff03bb62da2aecfc6ffa121dfd2bb20936392
gluggie <gluggie@29735491-9d32-0410-b17f-ed4c7e910dea>
2009-01-24 11:17:18 +0000

    Mac build script updated
    git-svn-id: https://usbpicprog.svn.sourceforge.net/svnroot/usbpicprog@516 29735491-9d32-0410-b17f-ed4c7e910dea

--- trunk/upp_wx/buildmac.sh
+++ trunk/upp_wx/buildmac.sh
@@ -1,4 +1,41 @@
+#!/bin/bash
+
+OUTPUTPATH=$(pwd)
+
 make clean
-./autogen.sh --with-macosx-sdk=/Developer/SDKs/MacOSX10.4u.sdk --with-macosx-version-min=10.4 --disable-debug
-make
-./mac_copytoapp.sh
+
+rm -rf src/usbpicprog.app
+mkdir -p src/usbpicprog.app
+mkdir -p src/usbpicprog.app/Contents
+mkdir -p src/usbpicprog.app/Contents/MacOS
+mkdir -p src/usbpicprog.app/Contents/Resources
+mkdir -p src/usbpicprog.app/Contents/SharedSupport
+mkdir -p src/usbpicprog.app/Contents/MacOS/output
+
+./configure --prefix=${OUTPUTPATH}/src/usbpicprog.app/Contents/MacOS/output
+make -j 2
+make install
+
+cp src/usbpicprog.app/Contents/MacOS/output/bin/usbpicprog src/usbpicprog.app/Contents/MacOS
+cp -r src/usbpicprog.app/Contents/MacOS/output/lib/locale src/usbpicprog.app/Contents/MacOS/po
+cp src/Info.plist src/usbpicprog.app/Contents
+cp icons/usbpicprog.icns src/usbpicprog.app/Contents/Resources
+echo -n "APPL????" > src/usbpicprog.app/Contents/PkgInfo
+
+rm -rf src/usbpicprog.app/Contents/MacOS/output
+
+EXECFILE="src/usbpicprog.app/Contents/MacOS/usbpicprog"
+LIBPATH="libs/"
+NEWLIBPATH="@executable_path/../SharedSupport"
+
+# space separated list of libraries
+TARGETS="libusb.dylib"
+for TARGET in ${TARGETS} ; do
+LIBFILE=${LIBPATH}/${TARGET}
+cp ${LIBFILE} src/usbpicprog.app/Contents/SharedSupport
+LIBFILE=src/usbpicprog.app/Contents/SharedSupport/${TARGET}
+TARGETID=`otool -DX ${LIBPATH}/$TARGET`
+NEWTARGETID=${NEWLIBPATH}/${TARGET}
+install_name_tool -id ${NEWTARGETID} ${LIBFILE}
+install_name_tool -change ${TARGETID} ${NEWTARGETID} ${EXECFILE}
+done
