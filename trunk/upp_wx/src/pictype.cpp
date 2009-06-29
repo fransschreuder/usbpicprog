926bfcab7310a518ffbad1b0fa66e2302fef06e2
fransschreuder <fransschreuder@29735491-9d32-0410-b17f-ed4c7e910dea>
2009-06-29 13:39:18 +0000

    fixed new pic family 12F61X
    git-svn-id: https://usbpicprog.svn.sourceforge.net/svnroot/usbpicprog@704 29735491-9d32-0410-b17f-ed4c7e910dea

--- trunk/upp_wx/src/pictype.cpp
+++ trunk/upp_wx/src/pictype.cpp
@@ -551,6 +551,7 @@ PicFamily PicType::GetFamilyFromString(const wxString& str)
     FAMILY(P24H);    
     FAMILY(P10F200);    
     FAMILY(P10F202);    
+	FAMILY(P12F61X);    
 
     return UPP_INVALID_PICFAMILY;
 }
