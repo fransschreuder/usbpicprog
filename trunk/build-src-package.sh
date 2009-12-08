e6b66da6f79c72aa6e828e505662571663b6e97a
fransschreuder <fransschreuder@29735491-9d32-0410-b17f-ed4c7e910dea>
2009-12-08 15:02:26 +0000

    created scripts for source package (launchpad ppa repository)
    git-svn-id: https://usbpicprog.svn.sourceforge.net/svnroot/usbpicprog@771 29735491-9d32-0410-b17f-ed4c7e910dea

---
+++ trunk/build-src-package.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+
+if [ $# -ne 0 ]
+then
+  RELEASE=$1
+else
+  RELEASE=$(svnversion -n)-$(date +%Y%m%d)
+fi
+echo Building release: $RELEASE
+#ARCHITECTURE=$(uname -m)
+#echo Building for architecture: $ARCHITECTURE
+
+if [ -d release ]
+then
+	echo Directory release exists, removing ...
+	rm -rf release
+fi
+echo making release directory
+mkdir release
+#mkdir release/usbpicprog
+#mkdir release/firmware
+
+echo exporting svn tree...
+#svn export uc_code release/firmware/firmware-$RELEASE
+#svn export boot release/firmware/boot1.0
+svn export upp_wx release/usbpicprog-$RELEASE
+
+#echo copy hexfiles...
+#cp release/firmware/firmware-$RELEASE/uc_code.hex release/firmware/firmware-$RELEASE.hex
+#cp release/firmware/boot1.0/Boot.hex release/firmware/boot1.0.hex
+
+#echo making firmware tar.gz archive...
+#cd release/firmware
+#tar -zcvhf firmware-$RELEASE.tar.gz *
+#mv firmware-$RELEASE.tar.gz ..
+#cd ..
+
+echo creating autogen files
+cd release/usbpicprog-$RELEASE
+./autogen.sh
+make distclean
+rm -rf m4
+rm -rf autom4te.cache
+if [ "$RELEASE" = "0.3.0" ]; then
+( echo '// generated file';
+    echo '#define UPP_VERSION "usbpicprog '$RELEASE'"'; )> svn_revision.h;
+else
+( echo '// generated file';
+    echo '#define SVN_REVISION "usbpicprog '$RELEASE'"'; )> svn_revision.h;
+fi
+
+cd ..
+echo creating source archive...
+tar -zcvhf usbpicprog_$RELEASE.orig.tar.gz usbpicprog-$RELEASE
+rm -rf usbpicprog-$RELEASE
+tar -zxvf usbpicprog_$RELEASE.orig.tar.gz
+cd usbpicprog-$RELEASE
+dh_make -e fransschreuder@gmail.com -s
+cp debian-src/* debian
+cd debian
+rm *.ex
+rm *.EX
+rm README.Debian
+cd ..
+debuild -S -k8AD5905E
+
