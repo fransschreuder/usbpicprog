6b9efecfbf750e550a6c1d385bd9d08b84503d56
neer <neer@29735491-9d32-0410-b17f-ed4c7e910dea>
2010-03-13 21:28:46 +0000

    Fixed the OSX-build scripts to create a correct installer image.
    git-svn-id: https://usbpicprog.svn.sourceforge.net/svnroot/usbpicprog@860 29735491-9d32-0410-b17f-ed4c7e910dea

--- trunk/upp_wx/osx/create_disk_image.sh
+++ trunk/upp_wx/osx/create_disk_image.sh
@@ -12,5 +12,12 @@ RELEASE=`svnversion -n`
 cd "$CURR_DIR"
 
 # Create the disk image 
-hdiutil create -srcfolder build/src/usbpicprog.app "$PREFIX_app/UsbPicProg-OSX-r$RELEASE.dmg"
+mkdir TMP
+cd TMP
+ln -s /Applications
+cd ..
+cp -r build/src/usbpicprog.app TMP/.
+yoursway-create-dmg-1.0.0.2/create-dmg --window-size 480 240 --volname UsbPicProg --background bgr.png --icon "usbpicprog.app" 120 120 --icon "Applications" 380 120 "$PREFIX_app/UsbPicProg-OSX-r$RELEASE.dmg" TMP/
 
+# Clean up
+rm -fr TMP
