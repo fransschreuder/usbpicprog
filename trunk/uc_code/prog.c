7021535808a495da89ec099f42b30c1a3d8ef806
fransschreuder <fransschreuder@29735491-9d32-0410-b17f-ed4c7e910dea>
2010-06-21 17:42:18 +0000

    cleaned up code for 10F devices
    git-svn-id: https://usbpicprog.svn.sourceforge.net/svnroot/usbpicprog@915 29735491-9d32-0410-b17f-ed4c7e910dea

--- trunk/uc_code/prog.c
+++ trunk/uc_code/prog.c
@@ -287,62 +287,20 @@ char bulk_erase(PICFAMILY picfamily,PICTYPE pictype,unsigned char doRestore)
 			break;
 		case P16F62X:
 			break;
-		case P10F200:
-			for(i=0;i<0xFF;i++)pic_send_n_bits(6,0x06);//set PC to FF
-			osccal=pic_read_14_bits(6,0x04);
-			for(i=0;i<5;i++)pic_send_n_bits(6,0x06);//set PC to 0x104
-			bandgap=pic_read_14_bits(6,0x04);
-			set_vdd_vpp(pictype, picfamily,0);
-			set_vdd_vpp(pictype, picfamily,1);
-			pic_send_n_bits(6,0x06);//increment address
-			pic_send_n_bits(6,0x09);//c) Execute Bulk Erase Program Memory (001001).
-			DelayMs(10);
-			set_vdd_vpp(pictype, picfamily,0);
-			set_vdd_vpp(pictype, picfamily,1);
-			for(i=0;i<0xFF;i++)pic_send_n_bits(6,0x06);//set PC to FF
-			pic_send_14_bits(6,0x04,osccal);
-			pic_send_n_bits(6,0x08); //begin programming
-			DelayMs(2);
-			pic_send_n_bits(6,0x0E); //end programming
-			DelayMs(1);
-			for(i=0;i<5;i++)pic_send_n_bits(6,0x06);//set PC to 0x104
-			pic_send_14_bits(6,0x04,bandgap);
-			pic_send_n_bits(6,0x08); //begin programming
-			DelayMs(2);
-			pic_send_n_bits(6,0x0E); //end programming
-			DelayMs(1);
-			break;
 		case P12F508:
 		case P16F59:
 		case P16F57:
 		case P16F54:
+			pic_send_n_bits(6,0x06); //increment address
 			pic_send_n_bits(6,0x09); //begin programming
 			DelayMs(20);
 			break;
+		case P10F200:
 		case P10F202:
-			for(i=0;i<0x1FF;i++)pic_send_n_bits(6,0x06);//set PC to 1FF
-			osccal=pic_read_14_bits(6,0x04);
-			for(i=0;i<5;i++)pic_send_n_bits(6,0x06);//set PC to 0x104
-			bandgap=pic_read_14_bits(6,0x04);
-			set_vdd_vpp(pictype, picfamily,0);
-			set_vdd_vpp(pictype, picfamily,1);
 			pic_send_n_bits(6,0x06);//increment address
 			pic_send_n_bits(6,0x09);//c) Execute Bulk Erase Program Memory (001001).
 			DelayMs(10);
-			set_vdd_vpp(pictype, picfamily,0);
-			set_vdd_vpp(pictype, picfamily,1);
-			for(i=0;i<0x1FF;i++)pic_send_n_bits(6,0x06);//set PC to 1FF
-			pic_send_14_bits(6,0x04,osccal);
-			pic_send_n_bits(6,0x08); //begin programming
-			DelayMs(2);
-			pic_send_n_bits(6,0x0E); //end programming
-			DelayMs(1);
-			for(i=0;i<5;i++)pic_send_n_bits(6,0x06);//set PC to 0x104
-			pic_send_14_bits(6,0x04,bandgap);
-			pic_send_n_bits(6,0x08); //begin programming
-			DelayMs(2);
-			pic_send_n_bits(6,0x0E); //end programming
-			DelayMs(1);
+			break;
 		default:
 			PGD=0;
 			set_vdd_vpp(pictype, picfamily,0);
@@ -689,34 +647,25 @@ char write_code(PICFAMILY picfamily, PICTYPE pictype, unsigned long address, uns
 		case P16F59:
 		case P10F200:
 		case P10F202:
-			if(lastblock&1)	pic_send_n_bits(6,0x06);//increment address to go from 1FF / 3FF to 0
+			if(lastblock&1)	
+			{
+				pic_send_n_bits(6,0x06);//increment address to go from 1FF / 3FF to 0
+				pic_send_n_bits(6,0x09);//bulk erase, which is necessary anyway...
+				DelayMs(20);
+			}
 			for(blockcounter=0;blockcounter<blocksize;blockcounter+=2)
 			{
 				pic_send_14_bits(6,0x02,(((unsigned int)data[blockcounter]))|   //MSB
 						(((unsigned int)data[blockcounter+1])<<8));//LSB
 				pic_send_n_bits(6,0x08);    //begin programming
-				DelayMs(2);
+				DelayMs(3);
 				pic_send_n_bits(6,0x0E);	//end programming
-				DelayMs(1);
+				DelayMs(2);
 				payload=pic_read_14_bits(6,0x04); //read code memory
 				if(payload!=((((unsigned int)data[blockcounter]))|(((unsigned int)data[blockcounter+1])<<8)))
-				{
-					for(i=0;i<10;i++)
-					{
-						//retry...
-						pic_send_14_bits(6,0x02,(((unsigned int)data[blockcounter]))|   //MSB
-								(((unsigned int)data[blockcounter+1])<<8));//LSB
-						pic_send_n_bits(6,0x08);    //begin programming
-						DelayMs(2);
-						pic_send_n_bits(6,0x0E);	//end programming
-						DelayMs(1);
-						payload=pic_read_14_bits(6,0x04); //read code memory
-						if(payload!=((((unsigned int)data[blockcounter]))|(((unsigned int)data[blockcounter+1])<<8)))
-						{
-							if(i==9)return 4; //verify error
-						}
-						else break; //verify successful, don't retry anymore
-					}
+				{ 
+					set_vdd_vpp(pictype,picfamily,0);
+					return 4;//verify error.
 				}
 				pic_send_n_bits(6,0x06);	//increment address
 			}
@@ -1109,7 +1058,7 @@ char write_config_bits(PICFAMILY picfamily, PICTYPE pictype, unsigned long addre
 			payload=(((unsigned int)data[0]))|(((unsigned int)data[1])<<8);
 			pic_send_14_bits(6,0x02,payload); //load data for programming
 			pic_send_n_bits(6,0x08);    //begin programming
-			DelayMs(2);
+			DelayMs(3);
 			pic_send_n_bits(6,0x0E);	//end programming
 			DelayMs(1);
 			break;
--- trunk/uc_code/svn_revision.h
+++ trunk/uc_code/svn_revision.h
@@ -1,2 +1,2 @@
 // generated file
-#define SVN_REVISION "usbpicprog 0.4.0"
+#define SVN_REVISION "usbpicprog 915"
