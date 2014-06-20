990935e5399364fa1a56bbfdeacd8230a27abe1c
Frans Schreuder <frans@frans-schreuders-dell-dxp061.local>
2014-06-20 19:02:06 +0000

    added x86_64 version for mac (universal contains now ppc, i386, x86_64)

--- trunk/upp_wx/osx/install_wxWidgets.sh
+++ trunk/upp_wx/osx/install_wxWidgets.sh
@@ -24,6 +24,12 @@ else
 	echo "Creating $PREFIX_i386"
 	mkdir -p "$PREFIX_i386"
 fi
+if [ -d "$PREFIX_x86_64" ]; then
+	echo "$PREFIX_x86_64 exists!"
+else 	
+	echo "Creating $PREFIX_x86_64"
+	mkdir -p "$PREFIX_x86_64"
+fi
 if [ -d "$PREFIX_app" ]; then
 	echo "$PREFIX_app exists!"
 else 	
@@ -42,20 +48,22 @@ ln -s /opt/local/bin/glibtool "$PREFIX_ppc"/bin/libtool
 ln -s /opt/local/bin/glibtoolize "$PREFIX_ppc"/bin/libtoolize
 ln -s /opt/local/bin/glibtool "$PREFIX_i386"/bin/libtool
 ln -s /opt/local/bin/glibtoolize "$PREFIX_i386"/bin/libtoolize
+ln -s /opt/local/bin/glibtool "$PREFIX_x86_64"/bin/libtool
+ln -s /opt/local/bin/glibtoolize "$PREFIX_x86_64"/bin/libtoolize
 
 # Download wxWidgets
 cd "$WORKDIR"
-if [ -f wxWidgets-2.9.4.tar.gz ]; then
-	if [ -d wxWidgets-2.9.4 ]; then
-		rm -rf wxWidgets-2.9.4	
+if [ -f wxWidgets-3.0.1.tar.bz2 ]; then
+	if [ -d wxWidgets-3.0.1 ]; then
+		rm -rf wxWidgets-3.0.1	
 	fi
 else
-	wget http://prdownloads.sourceforge.net/wxwindows/wxWidgets-2.9.4.tar.bz2
+	wget http://prdownloads.sourceforge.net/wxwindows/wxWidgets-3.0.1.tar.bz2
 fi
 # Unpack it
-tar xjf wxWidgets-2.9.4.tar.bz2
+tar xjf wxWidgets-3.0.1.tar.bz2
 # Get in there and create a builddir
-cd wxWidgets-2.9.4
+cd wxWidgets-3.0.1
 mkdir build-mac
 cd build-mac
 
@@ -81,6 +89,18 @@ make distclean
 # not the one bundled with Apple. THIS IS NECESSARY OR usbpicprog WILL FAIL
 # WHEN BUILDING!
 arch_flags="-arch i386 $sdk_flags"
+../configure "$qt_flags" CFLAGS="$arch_flags" CXXFLAGS="$arch_flags" CPPFLAGS="$arch_flags" LDFLAGS="$arch_flags" OBJCFLAGS="$arch_flags" OBJCXXFLAGS="$arch_flags" --disable-shared --with-libiconv-prefix=/opt/local/ --prefix="$PREFIX_i386" CC=gcc-4.0 CXX=g++-4.0 LD=g++-4.0
+make -j 2
+make install
+make distclean
+
+# Configure for x86_64 
+# NOTE: This is the magic line to make it work:
+# --with-libiconv-prefix=/opt/local/
+# This forces wxWidgets to use the MacPorts installation of libiconv and
+# not the one bundled with Apple. THIS IS NECESSARY OR usbpicprog WILL FAIL
+# WHEN BUILDING!
+arch_flags="-arch x86_64 $sdk_flags"
 ../configure "$qt_flags" CFLAGS="$arch_flags" CXXFLAGS="$arch_flags" CPPFLAGS="$arch_flags" LDFLAGS="$arch_flags" OBJCFLAGS="$arch_flags" OBJCXXFLAGS="$arch_flags" --disable-shared --with-libiconv-prefix=/opt/local/ --prefix="$PREFIX_i386" CC=gcc-4.0 CXX=g++-4.0 LD=g++-4.0
 make -j 2
 make install
